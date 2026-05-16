c29f706d0aabeeee3b76a42e1763113fb73bfdea
Maurício <mauricio.carvalho@mail.uft.edu.br>
2026-05-16 13:49:57 -0300

    ieee39_base.pwf convergiu

---
+++ anarede-executor-main/anarede-executor-main/README.md
@@ -0,0 +1,113 @@
+# Anarede  Executor
+*Python library to run ANAREDE PWF files using parallelism.*
+
+Este repositório contém um programa em Python para executar o ANAREDE em paralelo, aproveitando múltiplas instâncias do 
+executável para acelerar o processamento de arquivos de fluxo de carga.
+
+## ⚠️ Atenção
+
+> É necessário que o **ANAREDE esteja instalado** previamente em sua máquina. O executável `anarede.exe` deve estar 
+> disponível nos diretórios das instâncias paralelas.
+
+## ⚠️ Atenção
+
+> Na primeira execução, é necessário usar o comando `install_workers()` para criar as pastas das instâncias paralelas.
+> Consulte os exemplos.
+
+---
+
+## ✅ Pré-requisitos
+
+- Python 3.8 ou superior
+- Executável do ANAREDE disponível (e com permissão de execução)
+
+---
+
+## 📦 Instalação
+
+Recomenda-se instalar usando o pip:
+
+```bash
+pip install git+https://github.com/munizrodrigo/anarede-executor
+```
+
+Mas **apenas se você quiser ter acesso em um local específico da sua máquina**, clone este repositório:
+
+```bash
+git clone https://github.com/munizrodrigo/anarede-executor
+```
+
+Entre na pasta clonada e instale os requisitos:
+
+```bash
+pip install -r requirements.txt
+```
+
+---
+
+## 🚀 Uso
+
+### 🔧 Definindo arquivos e caminhos
+
+```python
+from anarede_executor import install_workers, is_installed, run_anarede # Importe as funções necessárias
+cases_dir = abspath(join(dirname(__file__), "cases"))
+pwf_files = [f"{cases_dir}\\case65\\case65-{str(i + 1).zfill(2)}\\case65.pwf" for i in range(30)] # Crie uma lista de arquivos a serem executados em paralelo
+```
+
+### 🧵 Execução paralela
+
+```python
+run_anarede(pwf_files, num_workers=8) # Execute os arquivos em paralelo usando 8 instâncias independentes
+```
+
+### 📌 Exemplo completo
+
+```python
+import time
+from os.path import dirname, join, abspath
+from anarede_executor import install_workers, is_installed, run_anarede
+
+
+if __name__ == "__main__":
+    if not is_installed():
+        install_workers()
+    else:
+        cases_dir = abspath(join(dirname(__file__), "cases"))
+        pwf_files = [f"{cases_dir}\\case65\\case65-{str(i + 1).zfill(2)}\\case65.pwf" for i in range(30)]
+
+        parallel_start_time = time.time()
+        run_anarede(pwf_files, num_workers=8)
+        parallel_end_time = time.time()
+        parallel_total_time = parallel_end_time - parallel_start_time
+
+        serial_start_time = time.time()
+        run_anarede(pwf_files, num_workers=1)
+        serial_end_time = time.time()
+        serial_total_time = serial_end_time - serial_start_time
+
+        print(f"Tempo gasto utilizando paralelismo: {parallel_total_time}s")
+        print(f"Tempo gasto utilizando execução serial: {serial_total_time}s")
+```
+
+---
+
+## 🧪 Testes
+
+> ⚠️ Recomendação: inicie os testes com **8 arquivos de entrada** e **8 instâncias**, monitorando se cada instância está sendo corretamente ativada e se os arquivos de saída não estão sendo sobrescritos.
+
+> ⚠️ Recomendação: Garanta que os arquivos .pwf não estejam direcionando seus relatórios para arquivos iguais.
+
+> ⚠️ Recomendação: O mais adequado é que cada arquivo .pwf a ser executado esteja sozinho em sua própria pasta, sendo assim não é ncessário se preocupar com os nomes dos relatórios gerados.
+
+---
+
+## 🙋 Contribuição
+
+Contribuições são bem-vindas! Sinta-se livre para abrir _issues_ ou enviar _pull requests_ com melhorias ou correções.
+
+---
+
+## 📄 Licença
+
+Este projeto está licenciado sob a licença MIT. Consulte o arquivo [LICENSE](LICENSE.md) para mais detalhes.
---
+++ anarede-executor-main/anarede-executor-main/src/anarede_executor/executor.py
@@ -0,0 +1,113 @@
+import os
+import time
+import subprocess
+from os.path import dirname, join, abspath
+from functools import partial
+
+import psutil
+from concurrent.futures import ProcessPoolExecutor
+
+
+MAX_NUM_WORKERS = round(0.75 * psutil.cpu_count())
+DEFAULT_TIMEOUT = 60.0
+MIN_CPU_USE_INTERVAL = 0.01
+DEFAULT_MAX_CPU_ZERO_COUNT = 50
+DEFAULT_WORKERS_DIR = os.environ.get("ANAREDE_WORKERS_DIR", None)
+
+
+def run_pwf_file(pair, num_workers=MAX_NUM_WORKERS, timeout=DEFAULT_TIMEOUT, cpu_use_interval=MIN_CPU_USE_INTERVAL,
+                 max_cpu_zero_count=DEFAULT_MAX_CPU_ZERO_COUNT, worker_dir=DEFAULT_WORKERS_DIR):
+    worker, anarede_path, pwf_file = pair
+    cmd = [anarede_path, pwf_file]
+    # print(f"Executando: {' '.join(cmd)}")
+
+    time.sleep((5 + worker) * cpu_use_interval)
+
+    files_in_dir = os.listdir(dirname(pwf_file))
+    for filename in files_in_dir:
+        if filename in ["pv.plt", "Qlim.out", "Relat.out"] or ".txt" in filename or ".TXT" in filename:
+            try:
+                os.remove(abspath(join(dirname(pwf_file), filename)))
+            except FileNotFoundError:
+                print(f"Arquivo {filename} nao encontrado.")
+    files_in_dir = os.listdir(dirname(pwf_file))
+
+    num_files_start = len(files_in_dir)
+
+    process = subprocess.Popen(
+        cmd
+    )
+    process_info = psutil.Process(process.pid)
+
+    time.sleep((2 + worker) * cpu_use_interval)
+
+    start = time.time()
+
+    try:
+        cpu_zero_count = -1
+        mark_for_restart = False
+        while True:
+            if process_info.cpu_percent(interval=cpu_use_interval) == 0.0:
+                if cpu_zero_count != -1:
+                    cpu_zero_count += 1
+            else:
+                cpu_zero_count = 0
+            elapsed_time = time.time() - start
+            if elapsed_time > timeout:
+                print(f"Timeout atingido. Encerrando processo {pwf_file}...")
+                process.kill()
+                process.wait()
+                if mark_for_restart:
+                    print(f"Reiniciando processo {pwf_file}...")
+                    return run_pwf_file(pair, num_workers=num_workers, timeout=timeout,
+                                        cpu_use_interval=cpu_use_interval, max_cpu_zero_count=max_cpu_zero_count,
+                                        worker_dir=worker_dir)
+                else:
+                    break
+            if cpu_zero_count > max_cpu_zero_count:
+                num_files = len(os.listdir(dirname(pwf_file)))
+                if num_files > num_files_start:
+                    print(f"CPU count atingido. Encerrando processo {pwf_file}...")
+                    process.kill()
+                    process.wait()
+                    break
+                else:
+                    print(f"CPU count atingido. Nenhum arquivo adicional detectado. Aguardando processo {pwf_file}...")
+                    cpu_zero_count -= 25
+                    mark_for_restart = True
+            time.sleep(cpu_use_interval)
+    except Exception as error:
+        raise error
+
+    time.sleep(2 * cpu_use_interval)
+
+    # return f"{pwf_file} finalizado com {anarede_path}"
+    return None
+
+
+def distribute_pwf_files(pwf_files, workers_paths):
+    pairs = []
+    for file_index, file in enumerate(pwf_files):
+        path = workers_paths[file_index % len(workers_paths)]
+        pairs.append((file_index, path, file))
+    return pairs
+
+
+def run_anarede(pwf_files, num_workers=MAX_NUM_WORKERS, timeout=DEFAULT_TIMEOUT, cpu_use_interval=MIN_CPU_USE_INTERVAL,
+                max_cpu_zero_count=DEFAULT_MAX_CPU_ZERO_COUNT, worker_dir=DEFAULT_WORKERS_DIR):
+    if worker_dir is None:
+        raise EnvironmentError("Workers do ANAREDE nao instalados")
+
+    workers_paths = [f"{worker_dir}\\w{i + 1}\\anarede.exe" for i in range(num_workers)]
+
+    pairs = distribute_pwf_files(pwf_files, workers_paths)
+
+    execute_anarede = partial(run_pwf_file, num_workers=num_workers, timeout=timeout,
+                              cpu_use_interval=cpu_use_interval, max_cpu_zero_count=max_cpu_zero_count,
+                              worker_dir=worker_dir)
+
+    with ProcessPoolExecutor(max_workers=num_workers) as executor:
+        results = executor.map(execute_anarede, pairs)
+
+    # for result in results:
+    #     print(result)
